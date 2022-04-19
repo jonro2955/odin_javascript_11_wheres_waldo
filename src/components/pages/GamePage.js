5f4c6377e2151f53b5a1c39b4fb265d91ef536d5
Jonathan Ro <jonro.2955@gmail.com>
2022-04-19 10:44:13 -0700

    Save the most recent clicked coordinates inside game image as a useState hook

--- src/components/pages/GamePage.js
+++ src/components/pages/GamePage.js
@@ -1,10 +1,17 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import planetsImg from '/home/pc/TOP/Projects/2_Full_Stack_JavaScript/odin_javascript_11_wheres_waldo/src/images/planets.jpg';
 import countriesImg from '/home/pc/TOP/Projects/2_Full_Stack_JavaScript/odin_javascript_11_wheres_waldo/src/images/countries.png';
 import gamesImg from '/home/pc/TOP/Projects/2_Full_Stack_JavaScript/odin_javascript_11_wheres_waldo/src/images/games.jpg';
 
 export default function GamePage() {
+  const clickedCoordsOutputDiv = document.getElementById('clickedCoordinates');
   const level = useParams().level;
+  const [lastCoord, setLastCoord] = useState({ x: '', y: '' });
+  useEffect(() => {
+    document.getElementById('clickedCoordinates').textContent =
+      lastCoord['x'] + ', ' + lastCoord['y'];
+  }, [lastCoord]);
 
   const findables = {
     planets: ['Mercury', 'Jupiter', 'Saturn'],
@@ -38,13 +45,13 @@ export default function GamePage() {
     }
   }
 
-  // Make mouse pointer become a circle insode gameImage
+  // Make mouse pointer become a circle inside game image
   function mouseEnterHandler() {
-    //The div with the .cursor class is styled to make it appear as a ring
+    //The div with the .ringCursor class is styled to make it look like a ring
     let ringCursor = document.querySelector('.ringCursor');
     ringCursor.style.display = 'initial';
     document.getElementById('gameImage').addEventListener('mousemove', (e) => {
-      //the above div's top and left style values are updated in response to mouse movements
+      //the ringCursor div's top and left style values are updated in response to mouse movements
       ringCursor.style.top = e.pageY + 'px';
       ringCursor.style.left = e.pageX + 'px';
     });
@@ -58,6 +65,9 @@ export default function GamePage() {
   // Open item selection menu at click location
   function clickHandler(e) {
     console.log(e.clientX, e.clientY);
+    setLastCoord({ x: e.clientX, y: e.clientY });
+    // document.getElementById('clickedCoordinates').textContent =
+    //   'Clicked Coordinates: ' + e.clientX + ', ' + e.clientY;
   }
 
   return (
@@ -73,10 +83,9 @@ export default function GamePage() {
         onMouseEnter={mouseEnterHandler}
         onMouseLeave={mouseLeaveHandler}
         onClick={clickHandler}
-        cursor='crosshair'
       ></img>
       <div className='ringCursor'></div>
-      <div>Found: </div>
+      <div id='clickedCoordinates'></div>
     </div>
   );
 }
