4f57b5e2440f651a9f0107170d8a416beb4332fd
Jonathan Ro <jonro.2955@gmail.com>
2022-04-19 00:40:48 -0700

    Change mouse pointer into a circle inside the <GamePage/>'s image element

--- src/components/pages/GamePage.js
+++ src/components/pages/GamePage.js
@@ -38,6 +38,24 @@ export default function GamePage() {
     }
   }
 
+  // Make mouse pointer become a circle insode gameImage
+  function mouseEnterHandler() {
+    //The div with the .cursor class is styled to make it appear as a ring
+    let ringCursor = document.querySelector('.ringCursor');
+    ringCursor.style.display = 'initial';
+    document.getElementById('gameImage').addEventListener('mousemove', (e) => {
+      //the above div's top and left style values are updated in response to mouse movements
+      ringCursor.style.top = e.pageY + 'px';
+      ringCursor.style.left = e.pageX + 'px';
+    });
+  }
+
+  function mouseLeaveHandler() {
+    let ringCursor = document.querySelector('.ringCursor');
+    ringCursor.style.display = 'none';
+  }
+
+  // Open item selection menu at click location
   function clickHandler(e) {
     console.log(e.clientX, e.clientY);
   }
@@ -50,9 +68,15 @@ export default function GamePage() {
         src={image()}
         alt={level}
         className='gameImage'
+        id='gameImage'
         width='1150'
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
         onClick={clickHandler}
+        cursor='crosshair'
       ></img>
+      <div className='ringCursor'></div>
+      <div>Found: </div>
     </div>
   );
 }
